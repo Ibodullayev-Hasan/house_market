5d1325b57905bc095a64469d86bada3687d2d1c3
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-03-22 17:42:14 +0500

    common va ichida guard va sercie lar yaratildi, guard va token.service lar toliq yozildi

---
+++ src/common/guards/role.guard.ts
@@ -0,0 +1,26 @@
+import { CanActivate, ExecutionContext } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { ROLES_KEY } from "src/decorators/roles.decorator";
+import { RolesEnum } from "src/enums";
+
+export class AuthGuard implements CanActivate {
+
+	constructor(private reflector: Reflector) { }
+
+	canActivate(context: ExecutionContext): boolean {
+		const requiredRoles = this.reflector.getAllAndOverride<RolesEnum[]>(
+			ROLES_KEY,
+			[
+				context.getHandler(),
+				context.getClass(),
+			]
+		)
+
+		if (!requiredRoles) return true
+
+		const { user } = context.switchToHttp().getRequest()
+		console.log(user);
+
+		return requiredRoles.some((el) => user.role?.includes(el))
+	}
+}
