37439edb617d297e169e47478a7f049179dcec83
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-03-23 06:36:12 +0500

    optimizatsiya qilinyabdi

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -2,15 +2,18 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@n
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
-
+import { AuthGuard } from 'src/common/guards/role.guard';
+import { Roles } from 'src/decorators/roles.decorator';
+import { RolesEnum } from 'src/enums/role.enum';
 
 @Controller('users')
-@UseGuards(JwtAuthGuard)
+@UseGuards(JwtAuthGuard, AuthGuard)
 export class UsersController {
 
   constructor(private readonly usersService: UsersService) { }
 
   @Get('all')
+  @Roles(RolesEnum.ADMIN)
   findAll() {
     return this.usersService.findAll();
   }
